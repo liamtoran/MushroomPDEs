104b0b221ac36cb657e9e2486c3eb82cea2e8752
liamkesatoran <bestofthefoo@gmail.com>
2019-05-24 11:33:28 +0200

    ajout obstacle pour l'edp 2d et methode iterative inversion matrice

--- edp_2d.py
+++ edp_2d.py
@@ -2,21 +2,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.sparse as sp
 from scipy.sparse.linalg.dsolve import spsolve
+from scipy.sparse.linalg import bicgstab, bicg, cg, cgs, gmres, lgmres, minres, qmr, gcrotmk
 import matplotlib.animation as animation
 from IPython.display import HTML
 
+import time
+start_time = time.time()
+
 #Coéfficients physiques
 K=.5 #coefficient diffusion
-b=.2 # dtC=-b*rho*C
+b=.2# dtC=-b*rho*C
 F0= 1 # dtRho = Fo*Mu
 
 physique = [K,b,F0]
 
 #Paramêtres numériques 
 n_t=1001 #nombre de pas de temps
-tf=40 # temps final de la simulation
-xf = 300 #longeur de la simulation
-n_x = 60 #nombres de points de la simulation
+tf=120 # temps final de la simulation
+xf = 500 #longueur de la simulation
+n_x = 200 #nombres de points de la simulation
 yf = xf
 n_y = n_x 
 n_xy = n_x * n_y
@@ -31,6 +35,18 @@ mu0=np.zeros(n_xy)#mu initial
 mu0[((n_xy+n_x)//2):((n_xy+n_x)//2)+1]=.01
 c0=np.zeros(n_xy)+1 #concentration initiale
 
+xm = 200
+xM = 300
+ym = 120
+yM = 220
+im = ((xm*n_x)//xf)
+iM = ((xM*n_x)//xf)
+jm = ((ym*n_y)//xf)
+jM = ((yM*n_y)//xf)
+for i in range(im,iM):
+    for j in range(jm,jM):
+        c0[i+j*n_x] = 0.
+        
 class EDP():
     def __init__(self,params):
         self.physique, self.numerique = params
@@ -51,12 +67,13 @@ class EDP():
         self.c0 = np.zeros(self.n_xy)
         
         #Matrice du Laplacien
-        Lapl = np.zeros((self.n_xy,self.n_xy))
-        Lapl += np.diag(-4*np.ones(n_xy),0)
-        Lapl += np.diag(np.ones(n_xy-1),1)+np.diag(np.ones(n_xy-1),-1)
-        Lapl += np.diag(np.ones(n_xy-n_y),n_y)+np.diag(np.ones(n_xy-n_y),-n_y)
-        self.Lapl = sp.csr_matrix(Lapl)
-        self.Lapl *= -self.K*self.dt/(self.dx**2)
+        Lapl = sp.diags(-4*np.ones(n_xy),0)
+        #Lapl += sp.diags(np.ones(n_xy-1),1)+sp.diags(np.ones(n_xy-1),-1)
+        diagmod = np.ones(n_xy-1)
+        diagmod[np.arange(n_y-1,n_xy-1,n_y)] = np.zeros(n_y-1)
+        Lapl += sp.diags(diagmod,1) + sp.diags(diagmod,-1)
+        Lapl += sp.diags(np.ones(n_xy-n_y),n_y)+sp.diags(np.ones(n_xy-n_y),-n_y)
+        self.Lapl = -self.K*self.dt/(self.dx**2)*Lapl
         
     def array_to_2D(n_x,vect):
         return np.array(np.split(vect,n_x))
@@ -67,7 +84,16 @@ class EDP():
         A = self.Lapl + sp.diags(alpha,0)
         Target =  mu+self.dt*c*rho
         
-        next_mu = spsolve(A,Target)
+        #next_mu = spsolve(A,Target) #95.28 secondes d'execution
+        #next_mu,check = bicg(A,Target) #3.38 secondes d'execution
+        next_mu,check = bicgstab(A,Target, x0=mu) #2.15 secondes d'execution
+        #next_mu,check = cg(A,Target) #2.29 secondes d'execution
+        #next_mu,check = cgs(A,Target) #2.36 secondes d'execution
+        #next_mu,check = gmres(A,Target) #2.72 secondes d'execution
+        #next_mu,check = lgmres(A,Target) #2.62 secondes d'execution
+        #next_mu,check = minres(A,Target) #2.15 secondes d'execution
+        #next_mu,check = qmr(A,Target) #3.70 secondes d'execution
+        #next_mu,check = gcrotmk(A,Target) #2.62 secondes d'execution
         next_rho = rho + self.dt*self.F0*next_mu
         next_c = c/(1+self.b*self.dt*next_rho)
         return next_mu, next_rho, next_c
@@ -90,12 +116,13 @@ while n<n_t:
     n+=1
     mu,rho,c= next_mu,next_rho,next_c
 
+print("--- %s seconds ---" % (time.time() - start_time))
 
 Draw = 'C' 
 f = C
   
 fig = plt.figure()    
-im = plt.imshow(EDP.array_to_2D(n_x,f[50]),cmap='hot', animated=True)
+im = plt.imshow(EDP.array_to_2D(n_x,f[50]), animated=True, cmap ='PiYG')
 
 i = 50
 def updatefig(*args):
@@ -107,5 +134,57 @@ def updatefig(*args):
 
 ani = animation.FuncAnimation(fig, updatefig, interval=50, blit=True, repeat=True)
 ani.save('EDP_2D_'+Draw+'.gif',writer='imagemagick', fps=30)
+
+Draw = 'Mu' 
+f = Mu
+  
+fig = plt.figure()    
+im = plt.imshow(EDP.array_to_2D(n_x,f[50]), animated=True, cmap ='PiYG')
+
+i = 50
+def updatefig(*args):
+    global i
+    i+=10
+    if i< n_t -1:
+        im.set_array(EDP.array_to_2D(n_x,f[i]))
+    return im,
+
+ani = animation.FuncAnimation(fig, updatefig, interval=50, blit=True, repeat=True)
+ani.save('EDP_2D_'+Draw+'.gif',writer='imagemagick', fps=30)
+
+Draw = 'LogMu' 
+f = np.log(Mu)
+  
+fig = plt.figure()    
+im = plt.imshow(EDP.array_to_2D(n_x,f[50]), animated=True, cmap ='PiYG')
+
+i = 50
+def updatefig(*args):
+    global i
+    i+=10
+    if i< n_t -1:
+        im.set_array(EDP.array_to_2D(n_x,f[i]))
+    return im,
+
+ani = animation.FuncAnimation(fig, updatefig, interval=50, blit=True, repeat=True)
+ani.save('EDP_2D_'+Draw+'.gif',writer='imagemagick', fps=30)
+
+Draw = 'Rho' 
+f = Rho
+  
+fig = plt.figure()    
+im = plt.imshow(EDP.array_to_2D(n_x,f[50]), animated=True, cmap ='PiYG')
+
+i = 50
+def updatefig(*args):
+    global i
+    i+=10
+    if i< n_t -1:
+        im.set_array(EDP.array_to_2D(n_x,f[i]))
+    return im,
+
+ani = animation.FuncAnimation(fig, updatefig, interval=50, blit=True, repeat=True)
+ani.save('EDP_2D_'+Draw+'.gif',writer='imagemagick', fps=30)
+
 
 plt.show()
