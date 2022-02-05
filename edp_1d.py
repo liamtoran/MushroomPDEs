345270313fac70ccc896c89592c298d844fbbce1
Liam Toran <liam.toran@gmail.com>
2022-02-04 20:41:08 -0800

    Lint .py files

--- edp_1d.py
+++ edp_1d.py
@@ -5,179 +5,211 @@ import scipy.sparse as sp
 from scipy.sparse.linalg.dsolve import spsolve
 import matplotlib.animation as animation
 
-#Coéfficients physiques
-K=.5 #coefficient diffusion
-b=.2 # dtC=-b*rho*C
-F0= 1 # dtRho = Fo*Mu
+# Coéfficients physiques
+K = 0.5  # coefficient diffusion
+b = 0.2  # dtC=-b*rho*C
+F0 = 1  # dtRho = Fo*Mu
 
-#Paramêtres numériques 
-n_t=3001 #nombre de pas de temps
-tf=20 # temps final de la simulation
-xf = 150 #longeur de la simulation
-n_x =601 #nombres de points de la simulation
+# Paramêtres numériques
+n_t = 3001  # nombre de pas de temps
+tf = 20  # temps final de la simulation
+xf = 150  # longeur de la simulation
+n_x = 601  # nombres de points de la simulation
 
-#Données initiales 
-rho0=np.zeros(n_x) #rho initial	
-mu0=np.zeros(n_x) #mu initial
-mu0[(n_x//2):(n_x//2 +10)]=.01
-c0=np.zeros(n_x)+1 #concentration initiale
-
-def edp_1d_explicite(K, b, F0, rho0, mu0, c0, n_t , tf, xf, n_x):
-    dt=tf/(n_t-1)
-    dx=xf/(n_x-1)
-    X=np.linspace(0,xf,n_x)
-    T=np.linspace(0,tf,n_t)
-    Mu=np.zeros((n_t,n_x))
-    Rho=np.zeros((n_t,n_x))
-    C=np.zeros((n_t,n_x))
-    Mu[0]=mu0
-    Rho[0]=rho0
-    C[0]=c0
-    #Résolution	du schema éxplicite
-    for n in range(0,n_t-1):
-        RHS=np.zeros(n_x)
-        alpha=-C[n]*dt*(1+dt*F0)+dt*Rho[n]+1
-        RHS[1:-1]= dt*((K/(dx**2))*(Mu[n,:-2]-2*Mu[n,1:-1]+Mu[n,2:])+C[n,1:-1]*Rho[n,1:-1])
-        RHS[0]= dt*((K/(dx**2))*(-2*Mu[n,0]+Mu[n,1])+C[n,0]*Rho[n,0])
-        RHS[-1]=dt*((K/(dx**2))*(-2*Mu[n,-1]+Mu[n,-2])+C[n,-1]*Rho[n,-1])
-        Mu[n+1]=(1/alpha)*(Mu[n]+RHS)
-        Rho[n+1]=Rho[n]+dt*F0*Mu[n+1]
-        C[n+1]=C[n]/(1 + b*dt*Rho[n+1])
-    return X,T,Mu,Rho,C
-
-def edp_1d_semi_implicite_I(K, b, F0, rho0, mu0, c0, n_t , tf, xf, n_x):
-    #Détermination des paramêtres numeriques deltat et deltax
-    dt=tf/(n_t-1)
-    dx=xf/(n_x-1)
-    #Représentation de l'éspace et du temps
-    X=np.linspace(0,xf,n_x)
-    T=np.linspace(0,tf,n_t)
-    #Initialisation
-    Mu=np.zeros((n_t,n_x))
-    Rho=np.zeros((n_t,n_x))
-    C=np.zeros((n_t,n_x))
-    Mu[0]=mu0
-    Rho[0]=rho0
-    C[0]=c0
-    #Résolution	du schéma implicite-explicite I
-    for n in range(0,n_t-1):
-        #Matrice du Laplacien
-        A=np.diag(-np.ones(n_x-1),-1)+np.diag(2*np.ones(n_x),0)+np.diag(-np.ones(n_x-1),1)
-        #Laplacien Numerique
-        A=A*K*dt/(dx**2)
-        #Ajout des termes implicites
-        alpha=-C[n]*dt*(1+dt*F0)+dt*Rho[n]+1
-        A+=np.diag(alpha,0)
-        A=sp.csc_matrix(A)
-        #Résolution du systême implicite
-        Mu[n+1]= spsolve(A, Mu[n]+dt*C[n]*Rho[n])
-        Rho[n+1]=Rho[n]+dt*F0*Mu[n+1]
-        C[n+1]=C[n]/(1 + b*dt*Rho[n+1])
-    return X,T,Mu,Rho,C
-    
-    
-def edp_1d_semi_implicite_II(K, b, F0, rho0, mu0, c0, n_t , tf, xf, n_x):
-    #Détermination des paramêtres numériques deltat et deltax
-    dt=tf/(n_t-1)
-    dx=xf/(n_x-1)
-    #Représentation de l'éspace et du temps
-    X=np.linspace(0,xf,n_x)
-    T=np.linspace(0,tf,n_t)
-    #Initialisation
-    Mu=np.zeros((n_t,n_x))
-    Rho=np.zeros((n_t,n_x))
-    C=np.zeros((n_t,n_x))
-    Mu[0]=mu0
-    Rho[0]=rho0
-    C[0]=c0
-    #Résolution	du schéma implicite-explicite II
-    for n in range(0,n_t-1):
-        #Matrice du Laplacien
-        A=np.diag(-np.ones(n_x-1),-1)+np.diag(2*np.ones(n_x),0)+np.diag(-np.ones(n_x-1),1)		
-        A=A*K*dt/(dx**2) #Laplacien Numerique
-        #Ajout des termes implicites
-        alpha=-C[n]*dt*(1+dt*F0)+1
-        A+=np.diag(alpha,0)
-        A= sp.csc_matrix(A)
-        #Résolution du systême implicite
-        Mu[n+1]= spsolve(A, Mu[n]+dt*C[n]*Rho[n]-dt*Mu[n]*Rho[n])
-        Rho[n+1]=Rho[n]+dt*F0*Mu[n+1]
-        C[n+1]=C[n]/(1 + b*dt*Rho[n+1])
-    return X,T,Mu,Rho,C
- #X,T,Mu,Rho,C= edp_1d_semi_implicite_I(K, b, F0, rho0, mu0, c0, n_t , tf, xf, n_x)
-
-X,T,Mu,Rho,C= edp_1d_semi_implicite_I(K, b, F0, rho0, mu0, c0, n_t , tf, xf, n_x)
+# Données initiales
+rho0 = np.zeros(n_x)  # rho initial
+mu0 = np.zeros(n_x)  # mu initial
+mu0[(n_x // 2) : (n_x // 2 + 10)] = 0.01
+c0 = np.zeros(n_x) + 1  # concentration initiale
 
 
-def speed(X,Rho,rho_inf):
-    #Position du front
-    argmed=np.zeros(n_t)
+def edp_1d_explicite(K, b, F0, rho0, mu0, c0, n_t, tf, xf, n_x):
+    dt = tf / (n_t - 1)
+    dx = xf / (n_x - 1)
+    X = np.linspace(0, xf, n_x)
+    T = np.linspace(0, tf, n_t)
+    Mu = np.zeros((n_t, n_x))
+    Rho = np.zeros((n_t, n_x))
+    C = np.zeros((n_t, n_x))
+    Mu[0] = mu0
+    Rho[0] = rho0
+    C[0] = c0
+    # Résolution	du schema éxplicite
+    for n in range(0, n_t - 1):
+        RHS = np.zeros(n_x)
+        alpha = -C[n] * dt * (1 + dt * F0) + dt * Rho[n] + 1
+        RHS[1:-1] = dt * (
+            (K / (dx**2)) * (Mu[n, :-2] - 2 * Mu[n, 1:-1] + Mu[n, 2:])
+            + C[n, 1:-1] * Rho[n, 1:-1]
+        )
+        RHS[0] = dt * (
+            (K / (dx**2)) * (-2 * Mu[n, 0] + Mu[n, 1]) + C[n, 0] * Rho[n, 0]
+        )
+        RHS[-1] = dt * (
+            (K / (dx**2)) * (-2 * Mu[n, -1] + Mu[n, -2]) + C[n, -1] * Rho[n, -1]
+        )
+        Mu[n + 1] = (1 / alpha) * (Mu[n] + RHS)
+        Rho[n + 1] = Rho[n] + dt * F0 * Mu[n + 1]
+        C[n + 1] = C[n] / (1 + b * dt * Rho[n + 1])
+    return X, T, Mu, Rho, C
+
+
+def edp_1d_semi_implicite_I(K, b, F0, rho0, mu0, c0, n_t, tf, xf, n_x):
+    # Détermination des paramêtres numeriques deltat et deltax
+    dt = tf / (n_t - 1)
+    dx = xf / (n_x - 1)
+    # Représentation de l'éspace et du temps
+    X = np.linspace(0, xf, n_x)
+    T = np.linspace(0, tf, n_t)
+    # Initialisation
+    Mu = np.zeros((n_t, n_x))
+    Rho = np.zeros((n_t, n_x))
+    C = np.zeros((n_t, n_x))
+    Mu[0] = mu0
+    Rho[0] = rho0
+    C[0] = c0
+    # Résolution	du schéma implicite-explicite I
+    for n in range(0, n_t - 1):
+        # Matrice du Laplacien
+        A = (
+            np.diag(-np.ones(n_x - 1), -1)
+            + np.diag(2 * np.ones(n_x), 0)
+            + np.diag(-np.ones(n_x - 1), 1)
+        )
+        # Laplacien Numerique
+        A = A * K * dt / (dx**2)
+        # Ajout des termes implicites
+        alpha = -C[n] * dt * (1 + dt * F0) + dt * Rho[n] + 1
+        A += np.diag(alpha, 0)
+        A = sp.csc_matrix(A)
+        # Résolution du systême implicite
+        Mu[n + 1] = spsolve(A, Mu[n] + dt * C[n] * Rho[n])
+        Rho[n + 1] = Rho[n] + dt * F0 * Mu[n + 1]
+        C[n + 1] = C[n] / (1 + b * dt * Rho[n + 1])
+    return X, T, Mu, Rho, C
+
+
+def edp_1d_semi_implicite_II(K, b, F0, rho0, mu0, c0, n_t, tf, xf, n_x):
+    # Détermination des paramêtres numériques deltat et deltax
+    dt = tf / (n_t - 1)
+    dx = xf / (n_x - 1)
+    # Représentation de l'éspace et du temps
+    X = np.linspace(0, xf, n_x)
+    T = np.linspace(0, tf, n_t)
+    # Initialisation
+    Mu = np.zeros((n_t, n_x))
+    Rho = np.zeros((n_t, n_x))
+    C = np.zeros((n_t, n_x))
+    Mu[0] = mu0
+    Rho[0] = rho0
+    C[0] = c0
+    # Résolution	du schéma implicite-explicite II
+    for n in range(0, n_t - 1):
+        # Matrice du Laplacien
+        A = (
+            np.diag(-np.ones(n_x - 1), -1)
+            + np.diag(2 * np.ones(n_x), 0)
+            + np.diag(-np.ones(n_x - 1), 1)
+        )
+        A = A * K * dt / (dx**2)  # Laplacien Numerique
+        # Ajout des termes implicites
+        alpha = -C[n] * dt * (1 + dt * F0) + 1
+        A += np.diag(alpha, 0)
+        A = sp.csc_matrix(A)
+        # Résolution du systême implicite
+        Mu[n + 1] = spsolve(A, Mu[n] + dt * C[n] * Rho[n] - dt * Mu[n] * Rho[n])
+        Rho[n + 1] = Rho[n] + dt * F0 * Mu[n + 1]
+        C[n + 1] = C[n] / (1 + b * dt * Rho[n + 1])
+    return X, T, Mu, Rho, C
+
+
+# X,T,Mu,Rho,C= edp_1d_semi_implicite_I(K, b, F0, rho0, mu0, c0, n_t , tf, xf, n_x)
+
+X, T, Mu, Rho, C = edp_1d_semi_implicite_I(K, b, F0, rho0, mu0, c0, n_t, tf, xf, n_x)
+
+
+def speed(X, Rho, rho_inf):
+    # Position du front
+    argmed = np.zeros(n_t)
     for i in range(n_t):
-        argmed[i]= X[(n_x//2)+ \
-            np.min(np.where(np.append(Rho[i,(n_x//2):],[0])<rho_inf/2))]
-    #Vitesse du front
-    s = ((n_t-1)/tf)*(argmed[(n_t//2)+150]-argmed[(n_t//2)])/(150)
+        argmed[i] = X[
+            (n_x // 2)
+            + np.min(np.where(np.append(Rho[i, (n_x // 2) :], [0]) < rho_inf / 2))
+        ]
+    # Vitesse du front
+    s = ((n_t - 1) / tf) * (argmed[(n_t // 2) + 150] - argmed[(n_t // 2)]) / (150)
     return s
 
-rho_inf = Rho[n_t-1,(n_x//2)]
-s = speed(X,Rho,rho_inf)
-print('La vitesse de propagation de la simulation est s=',s)
+
+rho_inf = Rho[n_t - 1, (n_x // 2)]
+s = speed(X, Rho, rho_inf)
+print("La vitesse de propagation de la simulation est s=", s)
 
 
 # Comparaison de s theorique et numerique pour plusieurs données initiales
-# ~ memory =[] 
+# ~ memory =[]
 # ~ for i in range(5):
-    # ~ for j in range(5):
-        # ~ K = .2 + .2*i
-        # ~ b = .1 + .1*j
-        # ~ X,T,Mu,Rho,C= edp_1d_semi_implicite_I(K, b, F0, rho0, mu0, c0, n_t , tf, xf, n_x)
-         # ~ #Valeur de rho a l'infini
-        # ~ rho_inf = Rho[n_t-1,(n_x//2)]
-        # ~ s = speed(X,Rho,rho_inf)
-        # ~ s_theorique = np.sqrt(K*((18*F0+4)+np.sqrt(((18*F0+4)**2)+108*(1+4*F0)*(F0**2)))/(2*(1+4*F0)))
-        # ~ memory += [K,b,s,s_theorique]
+# ~ for j in range(5):
+# ~ K = .2 + .2*i
+# ~ b = .1 + .1*j
+# ~ X,T,Mu,Rho,C= edp_1d_semi_implicite_I(K, b, F0, rho0, mu0, c0, n_t , tf, xf, n_x)
+# ~ #Valeur de rho a l'infini
+# ~ rho_inf = Rho[n_t-1,(n_x//2)]
+# ~ s = speed(X,Rho,rho_inf)
+# ~ s_theorique = np.sqrt(K*((18*F0+4)+np.sqrt(((18*F0+4)**2)+108*(1+4*F0)*(F0**2)))/(2*(1+4*F0)))
+# ~ memory += [K,b,s,s_theorique]
 # ~ np.savetxt('memory_data3.dat', memory)
 
 
-s_theorique = np.sqrt(K*((18*F0+4)+np.sqrt(((18*F0+4)**2)+108*(1+4*F0)*(F0**2)))/(2*(1+4*F0)))
-#Attention, ceci est pour C0=1
-print('La vitesse théorique de propagation est s_theorique=', s_theorique)
+s_theorique = np.sqrt(
+    K
+    * ((18 * F0 + 4) + np.sqrt(((18 * F0 + 4) ** 2) + 108 * (1 + 4 * F0) * (F0**2)))
+    / (2 * (1 + 4 * F0))
+)
+# Attention, ceci est pour C0=1
+print("La vitesse théorique de propagation est s_theorique=", s_theorique)
 
-#Animation
+# Animation
 fig = plt.figure()
 
-ax = plt.axes(xlim=(0, xf), ylim=(0, rho_inf+1))
-line, = ax.plot([], [], lw=2)
-line2, = ax.plot([], [], lw=2)
-line3, = ax.plot([], [], lw=2)
-line4, = ax.plot([], [], lw=2)
-time_text = ax.text(0.02, 0.92, '', transform=ax.transAxes)
-legend_text = ax.text(0.80, 0.82, '', transform=ax.transAxes)
+ax = plt.axes(xlim=(0, xf), ylim=(0, rho_inf + 1))
+(line,) = ax.plot([], [], lw=2)
+(line2,) = ax.plot([], [], lw=2)
+(line3,) = ax.plot([], [], lw=2)
+(line4,) = ax.plot([], [], lw=2)
+time_text = ax.text(0.02, 0.92, "", transform=ax.transAxes)
+legend_text = ax.text(0.80, 0.82, "", transform=ax.transAxes)
+
 
 def init():
     line.set_data([], [])
     line2.set_data([], [])
     line3.set_data([], [])
     line4.set_data([], [])
-    time_text.set_text('')
-    legend_text.set_text('')
-    return line,line2,line3,line4, time_text, legend_text
+    time_text.set_text("")
+    legend_text.set_text("")
+    return line, line2, line3, line4, time_text, legend_text
 
 
 def animate(i):
     line.set_data(X, C[i])
     line2.set_data(X, Rho[i])
     line3.set_data(X, Mu[i])
-    #line4.set_data(xf/2+((i*s)*tf/(n_t-1)),np.linspace(0,rho_inf+1,10))
-    time_text.set_text('time = {0:.1f}\n K={1}, b={2}, F0={3} '.format(T[i],K,b,F0))
-    legend_text.set_text('Rho=Orange \nMu=Green \nC=Blue\ns={0:.3f}'.format(s))
-    return line,line2, line3,line4, time_text, legend_text
+    # line4.set_data(xf/2+((i*s)*tf/(n_t-1)),np.linspace(0,rho_inf+1,10))
+    time_text.set_text("time = {0:.1f}\n K={1}, b={2}, F0={3} ".format(T[i], K, b, F0))
+    legend_text.set_text("Rho=Orange \nMu=Green \nC=Blue\ns={0:.3f}".format(s))
+    return line, line2, line3, line4, time_text, legend_text
 
 
-anim = animation.FuncAnimation(fig, animate, init_func=init,
-                               frames=(n_t-1), interval=(tf*200)/(n_t-1), blit=True)
-                               
+anim = animation.FuncAnimation(
+    fig,
+    animate,
+    init_func=init,
+    frames=(n_t - 1),
+    interval=(tf * 200) / (n_t - 1),
+    blit=True,
+)
 
 
-#anim.save('EDP_1D.gif',writer='imagemagick', fps=30)
+# anim.save('EDP_1D.gif',writer='imagemagick', fps=30)
 plt.show()
--- edp_2d.py
+++ edp_2d.py
@@ -1,185 +1,210 @@
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D 
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.animation as animation
 import numpy as np
 import scipy.sparse as sp
 from scipy.sparse.linalg.dsolve import spsolve
-from scipy.sparse.linalg import bicgstab, bicg, cg, cgs, gmres, lgmres, minres, qmr, gcrotmk
+from scipy.sparse.linalg import (
+    bicgstab,
+    bicg,
+    cg,
+    cgs,
+    gmres,
+    lgmres,
+    minres,
+    qmr,
+    gcrotmk,
+)
 from IPython.display import HTML
 import time
+
 start_time = time.time()
 
-#Coéfficients physiques
-K=.2 #coefficient diffusion
-b=.2# dtC=-b*rho*C
-F0= 1 # dtRho = Fo*Mu
+# Coéfficients physiques
+K = 0.2  # coefficient diffusion
+b = 0.2  # dtC=-b*rho*C
+F0 = 1  # dtRho = Fo*Mu
 
-physique = [K,b,F0]
+physique = [K, b, F0]
 
-#Paramêtres numériques 
-n_t=500 #nombre de pas de temps
-tf=170 # temps final de la simulation
-xf = 500 #longueur de la simulation
-n_x = 1100 #nombres de points de la simulation
+# Paramêtres numériques
+n_t = 500  # nombre de pas de temps
+tf = 170  # temps final de la simulation
+xf = 500  # longueur de la simulation
+n_x = 1100  # nombres de points de la simulation
 yf = xf
-n_y = n_x 
+n_y = n_x
 n_xy = n_x * n_y
-numerique = [n_t,tf,xf,n_x,yf,n_y]
+numerique = [n_t, tf, xf, n_x, yf, n_y]
 
-params = physique,numerique
+params = physique, numerique
 
 
-#Données initiales 
-rho0=np.zeros(n_xy) #rho initial	
-mu0=np.zeros(n_xy)#mu initial
-mu0[((n_xy+n_x)//2):((n_xy+n_x)//2)+1]=.01
-c0=np.zeros(n_xy) +1 #concentration initiale
+# Données initiales
+rho0 = np.zeros(n_xy)  # rho initial
+mu0 = np.zeros(n_xy)  # mu initial
+mu0[((n_xy + n_x) // 2) : ((n_xy + n_x) // 2) + 1] = 0.01
+c0 = np.zeros(n_xy) + 1  # concentration initiale
 
 xm = 200
 xM = 300
 ym = 120
 yM = 220
-im = ((xm*n_x)//xf)
-iM = ((xM*n_x)//xf)
-jm = ((ym*n_y)//xf)
-jM = ((yM*n_y)//xf)
-for i in range(im,iM):
-    for j in range(jm,jM):
-        c0[i+j*n_x] = 0.
-        
-class EDP():
-    def __init__(self,params):
+im = (xm * n_x) // xf
+iM = (xM * n_x) // xf
+jm = (ym * n_y) // xf
+jM = (yM * n_y) // xf
+for i in range(im, iM):
+    for j in range(jm, jM):
+        c0[i + j * n_x] = 0.0
+
+
+class EDP:
+    def __init__(self, params):
         self.physique, self.numerique = params
-        self.K,self.b,self.F0 = self.physique
-        self.n_t,self.tf,self.xf,self.n_x,self.yf,self.n_y = self.numerique
-        
-        
-        self.n_xy = self.n_x*self.n_y
-        self.dt = self.tf/(self.n_t-1)
-        self.dx = self.xf/(self.n_x-1)
-        self.dy = self.yf/(self.n_y-1)
-        
-        #self.X = np.linspace(0,self.xf,self.n_x)
-        #self.Y = np.linspace(0,self.yf,self.n_y)
-        #self.T = np.linspace(0,self.tf,self.n_t)
+        self.K, self.b, self.F0 = self.physique
+        self.n_t, self.tf, self.xf, self.n_x, self.yf, self.n_y = self.numerique
 
-        #Matrice du Laplacien
-        self.Lapl = sp.diags(-4*np.ones(self.n_xy),0)
-        #Lapl += sp.diags(np.ones(n_xy-1),1)+sp.diags(np.ones(n_xy-1),-1)
-        diagmod = np.ones(self.n_xy-1)
-        diagmod[np.arange(self.n_y-1,self.n_xy-1,self.n_y)] = np.zeros(self.n_y-1)
-        self.Lapl += sp.diags(diagmod,1) + sp.diags(diagmod,-1)
-        self.Lapl += sp.diags(np.ones(self.n_xy-self.n_y),self.n_y)+sp.diags(np.ones(self.n_xy-self.n_y),-self.n_y)
-        self.Lapl = -self.K*self.dt/(self.dx**2)*self.Lapl
+        self.n_xy = self.n_x * self.n_y
+        self.dt = self.tf / (self.n_t - 1)
+        self.dx = self.xf / (self.n_x - 1)
+        self.dy = self.yf / (self.n_y - 1)
+
+        # self.X = np.linspace(0,self.xf,self.n_x)
+        # self.Y = np.linspace(0,self.yf,self.n_y)
+        # self.T = np.linspace(0,self.tf,self.n_t)
+
+        # Matrice du Laplacien
+        self.Lapl = sp.diags(-4 * np.ones(self.n_xy), 0)
+        # Lapl += sp.diags(np.ones(n_xy-1),1)+sp.diags(np.ones(n_xy-1),-1)
+        diagmod = np.ones(self.n_xy - 1)
+        diagmod[np.arange(self.n_y - 1, self.n_xy - 1, self.n_y)] = np.zeros(
+            self.n_y - 1
+        )
+        self.Lapl += sp.diags(diagmod, 1) + sp.diags(diagmod, -1)
+        self.Lapl += sp.diags(np.ones(self.n_xy - self.n_y), self.n_y) + sp.diags(
+            np.ones(self.n_xy - self.n_y), -self.n_y
+        )
+        self.Lapl = -self.K * self.dt / (self.dx**2) * self.Lapl
         self.Cond = sp.identity(self.n_xy)
-    def array_to_2D(n_x,vect):
-        return np.array(np.split(vect,n_x))
 
-    def integrate(self,initial):
-        mu,rho,c = initial
-        alpha=-c*self.dt*(1+self.dt*self.F0)+self.dt*rho+1
-        A = self.Lapl + sp.diags(alpha,0)
-        Target =  mu+self.dt*c*rho
-        
-        #next_mu = spsolve(A,Target) #95.28 secondes d'execution
-        #next_mu,check = bicg(A,Target) #3.38 secondes d'execution
-        #next_mu,check = bicgstab(A,Target, x0=mu) #2.15 secondes d'execution
-        #next_mu,check = cg(A,Target) #2.29 secondes d'execution
-        #next_mu,check = cgs(A,Target) #2.36 secondes d'execution
-        #next_mu,check = gmres(A,Target) #2.72 secondes d'execution
-        #next_mu,check = lgmres(A,Target) #2.62 secondes d'execution
-        next_mu,check = minres(A,Target, x0=mu, M=self.Cond) #2.15 secondes d'execution
-        #next_mu,check = qmr(A,Target) #3.70 secondes d'execution
-        #next_mu,check = gcrotmk(A,Target) #2.62 secondes d'execution
-        next_rho = rho + self.dt*self.F0*next_mu
-        next_c = c/(1+self.b*self.dt*next_rho)
+    def array_to_2D(n_x, vect):
+        return np.array(np.split(vect, n_x))
+
+    def integrate(self, initial):
+        mu, rho, c = initial
+        alpha = -c * self.dt * (1 + self.dt * self.F0) + self.dt * rho + 1
+        A = self.Lapl + sp.diags(alpha, 0)
+        Target = mu + self.dt * c * rho
+
+        # next_mu = spsolve(A,Target) #95.28 secondes d'execution
+        # next_mu,check = bicg(A,Target) #3.38 secondes d'execution
+        # next_mu,check = bicgstab(A,Target, x0=mu) #2.15 secondes d'execution
+        # next_mu,check = cg(A,Target) #2.29 secondes d'execution
+        # next_mu,check = cgs(A,Target) #2.36 secondes d'execution
+        # next_mu,check = gmres(A,Target) #2.72 secondes d'execution
+        # next_mu,check = lgmres(A,Target) #2.62 secondes d'execution
+        next_mu, check = minres(
+            A, Target, x0=mu, M=self.Cond
+        )  # 2.15 secondes d'execution
+        # next_mu,check = qmr(A,Target) #3.70 secondes d'execution
+        # next_mu,check = gcrotmk(A,Target) #2.62 secondes d'execution
+        next_rho = rho + self.dt * self.F0 * next_mu
+        next_c = c / (1 + self.b * self.dt * next_rho)
         return next_mu, next_rho, next_c
-        
-        
+
+
 Agent = EDP(params)
 dt = Agent.dt
 
-mu= mu0
-rho= rho0
-c= c0
-Mu=[mu0]
-Rho=[rho0]
-C=[c0]
-T=[0]
+mu = mu0
+rho = rho0
+c = c0
+Mu = [mu0]
+Rho = [rho0]
+C = [c0]
+T = [0]
 n = 0
 step = 5
-while n<n_t:
-    mu,rho,c = Agent.integrate((mu,rho,c)) 
-    if n % step ==0 :
+while n < n_t:
+    mu, rho, c = Agent.integrate((mu, rho, c))
+    if n % step == 0:
         Mu.append(mu)
         Rho.append(rho)
         C.append(c)
-        T.append(n*dt)
-    if n % 25 ==0 :
+        T.append(n * dt)
+    if n % 25 == 0:
         print(n, (time.time() - start_time))
-    n+=1
+    n += 1
 
 print("--- %s seconds ---" % (time.time() - start_time))
 
 tot = len(Mu)
 
-Draw = 'C' 
+Draw = "C"
 f = C
-  
-fig = plt.figure()    
-im = plt.imshow(EDP.array_to_2D(n_x,f[2]), animated=True, cmap ='PiYG')
-time_text = plt.text(0, 0, '')
+
+fig = plt.figure()
+im = plt.imshow(EDP.array_to_2D(n_x, f[2]), animated=True, cmap="PiYG")
+time_text = plt.text(0, 0, "")
 
 i = 2
+
+
 def updatefig(*args):
     global i
-    i+=1
-    if i< tot -1:
-        im.set_array(EDP.array_to_2D(n_x,f[i]))
-        time_text.set_text('time = {0:.1f}'.format(T[i]))
+    i += 1
+    if i < tot - 1:
+        im.set_array(EDP.array_to_2D(n_x, f[i]))
+        time_text.set_text("time = {0:.1f}".format(T[i]))
     return im, time_text
 
-ani = animation.FuncAnimation(fig, updatefig, interval=100, blit=True, repeat=True)
-ani.save('EDP_2D_'+Draw+'.gif',writer='imagemagick', fps=30)
 
-Draw = 'Mu' 
+ani = animation.FuncAnimation(fig, updatefig, interval=100, blit=True, repeat=True)
+ani.save("EDP_2D_" + Draw + ".gif", writer="imagemagick", fps=30)
+
+Draw = "Mu"
 f = Mu
-  
-fig = plt.figure()    
-im = plt.imshow(EDP.array_to_2D(n_x,f[2]), animated=True, cmap ='PiYG')
-time_text = plt.text(0, 0, 'test')
+
+fig = plt.figure()
+im = plt.imshow(EDP.array_to_2D(n_x, f[2]), animated=True, cmap="PiYG")
+time_text = plt.text(0, 0, "test")
 i = 2
+
+
 def updatefig(*args):
     global i
-    i+=1
-    if i< tot -1:
-        im.set_array(EDP.array_to_2D(n_x,f[i]))
-        time_text.set_text('time = {0:.1f}'.format(T[i]))
+    i += 1
+    if i < tot - 1:
+        im.set_array(EDP.array_to_2D(n_x, f[i]))
+        time_text.set_text("time = {0:.1f}".format(T[i]))
     return im, time_text
 
+
 ani = animation.FuncAnimation(fig, updatefig, interval=100, blit=True, repeat=True)
-ani.save('EDP_2D_'+Draw+'.gif',writer='imagemagick', fps=30)
+ani.save("EDP_2D_" + Draw + ".gif", writer="imagemagick", fps=30)
 
 
-Draw = 'Rho' 
+Draw = "Rho"
 f = Rho
-  
-fig = plt.figure()    
-im = plt.imshow(EDP.array_to_2D(n_x,f[2]), animated=True, cmap ='PiYG')
-time_text = plt.text(0, 0, '')
+
+fig = plt.figure()
+im = plt.imshow(EDP.array_to_2D(n_x, f[2]), animated=True, cmap="PiYG")
+time_text = plt.text(0, 0, "")
 i = 2
+
+
 def updatefig(*args):
     global i
-    i+=1
-    if i< tot -1:
-        im.set_array(EDP.array_to_2D(n_x,f[i]))
-        time_text.set_text('time = {0:.1f}'.format(T[i]))
+    i += 1
+    if i < tot - 1:
+        im.set_array(EDP.array_to_2D(n_x, f[i]))
+        time_text.set_text("time = {0:.1f}".format(T[i]))
     return im, time_text
 
+
 ani = animation.FuncAnimation(fig, updatefig, interval=100, blit=True, repeat=True)
-ani.save('EDP_2D_'+Draw+'.gif',writer='imagemagick', fps=30)
+ani.save("EDP_2D_" + Draw + ".gif", writer="imagemagick", fps=30)
 
 
 plt.show()
-
-
